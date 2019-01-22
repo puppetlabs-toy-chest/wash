1b078f6172de2d397255b17119769d4c30327c16
Michael Smith <michael.smith@puppet.com>
2019-01-22 11:35:27 -0800

    Store cache entry for each pod to reduce memory and decoding overhead
    Makes listing pods much faster.

--- kubernetes/core.go
+++ kubernetes/core.go
@@ -73,50 +73,89 @@ func (cli *Client) log(format string, v ...interface{}) {
 	}
 }
 
-func (cli *Client) cachedPodList(ctx context.Context) (map[string]corev1.Pod, error) {
-	entry, err := cli.cache.Get("PodList")
-	var pods map[string]corev1.Pod
-	if err == nil {
-		cli.log("Cache hit in /kubernetes")
-		dec := gob.NewDecoder(bytes.NewReader(entry))
-		err = dec.Decode(&pods)
-	} else {
-		cli.log("Cache miss in /kubernetes")
-
-		podList, err := cli.CoreV1().Pods("").List(metav1.ListOptions{})
-		if err != nil {
-			return nil, err
-		}
-
-		pods = make(map[string]corev1.Pod)
-		for _, pod := range podList.Items {
-			pods[string(pod.UID)] = pod
-		}
-
-		var data bytes.Buffer
-		enc := gob.NewEncoder(&data)
-		if err := enc.Encode(&pods); err != nil {
-			return nil, err
-		}
-		cli.cache.Set("PodList", data.Bytes())
-		cli.updated = time.Now()
+func (cli *Client) updateCachedPods(ctx context.Context) error {
+	podList, err := cli.CoreV1().Pods("").List(metav1.ListOptions{})
+	if err != nil {
+		return err
 	}
+
+	var data bytes.Buffer
+
+	podNames := make([]string, len(podList.Items))
+	for i, pod := range podList.Items {
+		enc := gob.NewEncoder(&data)
+		if err := enc.Encode(pod); err != nil {
+			return err
+		}
+		cli.cache.Set(string(pod.UID), data.Bytes())
+		data.Reset()
+		podNames[i] = string(pod.UID)
+	}
+
+	enc := gob.NewEncoder(&data)
+	if err := enc.Encode(&podNames); err != nil {
+		return err
+	}
+	cli.cache.Set("PodList", data.Bytes())
+
+	return nil
+}
+
+func (cli *Client) cachedPodList(ctx context.Context) ([]string, error) {
+	entry, err := cli.cache.Get("PodList")
+	if err != nil {
+		cli.log("Cache miss in /kubernetes")
+		if err := cli.updateCachedPods(ctx); err != nil {
+			return nil, err
+		}
+		if entry, err = cli.cache.Get("PodList"); err != nil {
+			return nil, err
+		}
+	} else {
+		cli.log("Cache hit in /kubernetes")
+	}
+
+	var pods []string
+	dec := gob.NewDecoder(bytes.NewReader(entry))
+	err = dec.Decode(&pods)
 	return pods, err
+}
+
+func (cli *Client) cachedPodFind(ctx context.Context, name string) (*corev1.Pod, error) {
+	entry, err := cli.cache.Get(name)
+	if err != nil {
+		// If name wasn't found, check whether PodList was loaded and if not load it.
+		if _, err := cli.cache.Get("PodList"); err != nil {
+			cli.log("Cache miss in /kubernetes/%v", name)
+			if err := cli.updateCachedPods(ctx); err != nil {
+				return nil, err
+			}
+			entry, err = cli.cache.Get(name)
+		}
+
+		if err != nil {
+			// Name wasn't found after refreshing PodList cache (or cache was up-to-date).
+			return nil, err
+		}
+	} else {
+		cli.log("Cache hit in /kubernetes/%v", name)
+	}
+
+	var pod corev1.Pod
+	dec := gob.NewDecoder(bytes.NewReader(entry))
+	err = dec.Decode(&pod)
+	return &pod, err
 }
 
 // Find container by ID.
 func (cli *Client) Find(ctx context.Context, name string) (*plugin.Entry, error) {
-	pods, err := cli.cachedPodList(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	if pod, ok := pods[name]; ok {
+	if pod, err := cli.cachedPodFind(ctx, name); err == nil {
 		cli.log("Found pod %v, %v", name, pod)
 		return &plugin.Entry{Client: cli, Name: name}, nil
+	} else {
+		cli.log("Pod %v not found: %v", name, err)
+		return nil, plugin.ENOENT
 	}
-	cli.log("Pod %v not found", name)
-	return nil, plugin.ENOENT
 }
 
 // List all running pods as files.
@@ -128,7 +167,7 @@ func (cli *Client) List(ctx context.Context) ([]plugin.Entry, error) {
 	cli.log("Listing %v pods in /kubernetes", len(pods))
 	keys := make([]plugin.Entry, 0, len(pods))
 	for _, pod := range pods {
-		keys = append(keys, plugin.Entry{Client: cli, Name: string(pod.UID)})
+		keys = append(keys, plugin.Entry{Client: cli, Name: pod})
 	}
 	return keys, nil
 }
