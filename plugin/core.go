abe373e83e26cda98228210125df40a578f355c4
Michael Smith <michael.smith@puppet.com>
2019-01-28 15:35:01 -0800

    Allow plugins to opt-in to more detailed printing
    If DirProtocol or FileProtocol implement `String()`, use that; otherwise
    use `Name()` for logging. Add additional logging when errors occur.

--- plugin/core.go
+++ plugin/core.go
@@ -76,15 +76,25 @@ func NewDir(impl DirProtocol) *Dir {
 	return &Dir{impl}
 }
 
+func (d *Dir) String() string {
+	if v, ok := d.DirProtocol.(interface{ String() string }); ok {
+		return v.String()
+	}
+	return d.Name()
+}
+
 // Attr returns the attributes of a directory.
 func (d *Dir) Attr(ctx context.Context, a *fuse.Attr) error {
 	a.Mode = os.ModeDir | 0550
 
 	attr, err := d.DirProtocol.Attr(ctx)
+	if err != nil {
+		log.Printf("Error[Attr,%v]: %v", d, err)
+	}
 	a.Mtime = attr.Mtime
 	a.Size = attr.Size
 	a.Valid = attr.Valid
-	log.Printf("Attr of dir %v: %v, %v", d.Name(), a.Mtime, a.Size)
+	log.Printf("Attr of dir %v: %v, %v", d, a.Mtime, a.Size)
 	return err
 }
 
@@ -97,6 +107,7 @@ func (d *Dir) Lookup(ctx context.Context, req *fuse.LookupRequest, resp *fuse.Lo
 func (d *Dir) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
 	entries, err := d.List(ctx)
 	if err != nil {
+		log.Printf("Error[List,%v]: %v", d, err)
 		return nil, err
 	}
 
@@ -120,15 +131,25 @@ func NewFile(impl FileProtocol) *File {
 	return &File{impl}
 }
 
+func (f *File) String() string {
+	if v, ok := f.FileProtocol.(interface{ String() string }); ok {
+		return v.String()
+	}
+	return f.Name()
+}
+
 // Attr returns the attributes of a file.
 func (f *File) Attr(ctx context.Context, a *fuse.Attr) error {
 	a.Mode = 0440
 
 	attr, err := f.FileProtocol.Attr(ctx)
+	if err != nil {
+		log.Printf("Error[Attr,%v]: %v", f, err)
+	}
 	a.Mtime = attr.Mtime
 	a.Size = attr.Size
 	a.Valid = attr.Valid
-	log.Printf("Attr of file %v: %v, %s", f.Name(), a.Size, a.Mtime)
+	log.Printf("Attr of file %v: %v, %s", f, a.Size, a.Mtime)
 	return err
 }
 
@@ -136,6 +157,7 @@ func (f *File) Attr(ctx context.Context, a *fuse.Attr) error {
 func (f *File) Listxattr(ctx context.Context, req *fuse.ListxattrRequest, resp *fuse.ListxattrResponse) error {
 	xattrs, err := f.Xattr(ctx)
 	if err != nil {
+		log.Printf("Error[Xattr,%v]: %v", f, err)
 		return err
 	}
 
@@ -149,6 +171,7 @@ func (f *File) Listxattr(ctx context.Context, req *fuse.ListxattrRequest, resp *
 func (f *File) Getxattr(ctx context.Context, req *fuse.GetxattrRequest, resp *fuse.GetxattrResponse) error {
 	xattrs, err := f.Xattr(ctx)
 	if err != nil {
+		log.Printf("Error[Xattr,%v]: %v", f, err)
 		return err
 	}
 
@@ -159,12 +182,13 @@ func (f *File) Getxattr(ctx context.Context, req *fuse.GetxattrRequest, resp *fu
 // Open a file for reading.
 func (f *File) Open(ctx context.Context, req *fuse.OpenRequest, resp *fuse.OpenResponse) (fs.Handle, error) {
 	// Initiate content request and return a channel providing the results.
-	log.Printf("Opening %v", f.Name())
+	log.Printf("Opening %v", f)
 	r, err := f.FileProtocol.Open(ctx)
 	if err != nil {
+		log.Printf("Error[Open,%v]: %v", f, err)
 		return nil, err
 	}
-	log.Printf("Opened %v", f.Name())
+	log.Printf("Opened %v", f)
 	return &FileHandle{r: r}, nil
 }
 
