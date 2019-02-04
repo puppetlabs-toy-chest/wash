1daea83bcc08192e64f290e462cbc3c1c4f6af4f
Michael Smith <michael.smith@puppet.com>
2019-02-04 12:39:44 -0800

    Add reading files from a container
    Note that reading deep into a hierarchy is currently expensive. May need
    to come up with ways to batch multi-level lookups when searching a path.

--- docker/volume.go
+++ docker/volume.go
@@ -1,6 +1,7 @@
 package docker
 
 import (
+	"archive/tar"
 	"bufio"
 	"bytes"
 	"context"
@@ -12,13 +13,13 @@ import (
 	"path"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/docker/docker/api/types"
 	docontainer "github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/mount"
 	"github.com/docker/docker/api/types/network"
-	"github.com/docker/docker/api/types/strslice"
 	"github.com/puppetlabs/wash/datastore"
 	"github.com/puppetlabs/wash/log"
 	"github.com/puppetlabs/wash/plugin"
@@ -30,10 +31,11 @@ type volume struct {
 	name string
 	path string
 	attr plugin.Attributes
+	mux  sync.Mutex
 }
 
 func newVolume(cli *resourcetype, name string) *volume {
-	return &volume{cli, name, "", plugin.Attributes{}}
+	return &volume{cli, name, "", plugin.Attributes{}, sync.Mutex{}}
 }
 
 func (cli *volume) Find(ctx context.Context, name string) (plugin.Node, error) {
@@ -43,7 +45,7 @@ func (cli *volume) Find(ctx context.Context, name string) (plugin.Node, error) {
 	}
 
 	if attr, ok := attrs[name]; ok {
-		newvol := &volume{cli.resourcetype, cli.name, cli.path + "/" + name, attr}
+		newvol := &volume{cli.resourcetype, cli.name, cli.path + "/" + name, attr, sync.Mutex{}}
 		if attr.Mode.IsDir() {
 			return plugin.NewDir(newvol), nil
 		}
@@ -65,7 +67,7 @@ func (cli *volume) List(ctx context.Context) ([]plugin.Node, error) {
 			continue
 		}
 
-		newvol := &volume{cli.resourcetype, cli.name, cli.path + "/" + entry, attr}
+		newvol := &volume{cli.resourcetype, cli.name, cli.path + "/" + entry, attr, sync.Mutex{}}
 		if attr.Mode.IsDir() {
 			entries = append(entries, plugin.NewDir(newvol))
 		} else {
@@ -76,7 +78,7 @@ func (cli *volume) List(ctx context.Context) ([]plugin.Node, error) {
 }
 
 func (cli *volume) String() string {
-	return cli.resourcetype.String() + "/" + cli.Name() + cli.path
+	return cli.resourcetype.String() + "/" + cli.name + cli.path
 }
 
 func (cli *volume) Name() string {
@@ -112,9 +114,13 @@ func (cli *volume) Xattr(ctx context.Context) (map[string][]byte, error) {
 
 // TODO: is it a good idea to mix functions? Are NewDir and NewFile enough to differentiate?
 func (cli *volume) Open(ctx context.Context) (plugin.IFileBuffer, error) {
-	return nil, plugin.ENOTSUP
+	cli.mux.Lock()
+	defer cli.mux.Unlock()
+	return cli.cachedContent(ctx)
 }
 
+// TODO: the stat format, parsing it, and starting a container to run a command is almost certainly
+// useful other places as well. Find a place to put them.
 func parseStat(line string) (plugin.Attributes, string, error) {
 	var attr plugin.Attributes
 	segments := strings.SplitN(line, " ", 6)
@@ -174,6 +180,8 @@ func statFormat() string {
 	return "%s %X %Y %Z %f %n"
 }
 
+const mountpoint = "/mnt"
+
 func (cli *volume) cachedAttributes(ctx context.Context) (map[string]plugin.Attributes, error) {
 	key := cli.String() + "/list"
 	entry, err := cli.Get(key)
@@ -189,26 +197,13 @@ func (cli *volume) cachedAttributes(ctx context.Context) (map[string]plugin.Attr
 	log.Printf("Cache miss on %v", key)
 
 	// Create a container that mounts a volume and inspects it. Run it and capture the output.
-	cmd := strslice.StrSlice{"sh", "-c", "stat -c '" + statFormat() + "' /mnt" + cli.path + "/.* /mnt" + cli.path + "/*"}
-	// Use tty to avoid messing with the extra log formatting.
-	cfg := docontainer.Config{Image: "busybox", Cmd: cmd, Tty: true}
-	mounts := []mount.Mount{mount.Mount{
-		Type:     mount.TypeVolume,
-		Source:   cli.name,
-		Target:   "/mnt",
-		ReadOnly: true,
-	}}
-	hostcfg := docontainer.HostConfig{Mounts: mounts}
-	netcfg := network.NetworkingConfig{}
-	created, err := cli.ContainerCreate(ctx, &cfg, &hostcfg, &netcfg, "")
+	cmd := []string{"sh", "-c", "stat -c '" + statFormat() + "' " + mountpoint + cli.path + "/.* " + mountpoint + cli.path + "/*"}
+	cid, err := cli.createContainer(ctx, cmd)
 	if err != nil {
 		return nil, err
 	}
-	for _, warn := range created.Warnings {
-		log.Debugf("Warning creating %v: %v", key, warn)
-	}
-	cid := created.ID
 	defer cli.ContainerRemove(ctx, cid, types.ContainerRemoveOptions{})
+
 	log.Debugf("Starting container %v", cid)
 	if err := cli.ContainerStart(ctx, cid, types.ContainerStartOptions{}); err != nil {
 		return nil, err
@@ -267,4 +262,72 @@ func (cli *volume) cachedAttributes(ctx context.Context) (map[string]plugin.Attr
 	cli.updated = time.Now()
 	err = datastore.CacheAny(cli.BigCache, key, attrs)
 	return attrs, err
+}
+
+func (cli *volume) cachedContent(ctx context.Context) (plugin.IFileBuffer, error) {
+	key := cli.String() + "/content"
+	entry, err := cli.Get(key)
+	if err == nil {
+		log.Debugf("Cache hit on %v", key)
+		return bytes.NewReader(entry), nil
+	}
+
+	// Cache misses should be rarer, so always print them. Frequent messages are a sign of problems.
+	log.Printf("Cache miss on %v", key)
+
+	// Create a container that mounts a volume and waits. Use it to download a file.
+	cid, err := cli.createContainer(ctx, []string{"sleep", "60"})
+	if err != nil {
+		return nil, err
+	}
+	defer cli.ContainerRemove(ctx, cid, types.ContainerRemoveOptions{})
+
+	log.Debugf("Starting container %v", cid)
+	if err := cli.ContainerStart(ctx, cid, types.ContainerStartOptions{}); err != nil {
+		return nil, err
+	}
+	defer cli.ContainerKill(ctx, cid, "SIGKILL")
+
+	// Download file, then kill container.
+	rdr, _, err := cli.CopyFromContainer(ctx, cid, mountpoint+cli.path)
+	if err != nil {
+		return nil, err
+	}
+	defer rdr.Close()
+
+	tarReader := tar.NewReader(rdr)
+	// Only expect one file.
+	if _, err := tarReader.Next(); err != nil {
+		return nil, err
+	}
+	bits, err := ioutil.ReadAll(tarReader)
+	if err != nil {
+		return nil, err
+	}
+
+	cli.updated = time.Now()
+	cli.Set(key, bits)
+	return bytes.NewReader(bits), nil
+}
+
+// Create a container that mounts a volume to a default mountpoint and runs a command.
+func (cli *volume) createContainer(ctx context.Context, cmd []string) (string, error) {
+	// Use tty to avoid messing with the extra log formatting.
+	cfg := docontainer.Config{Image: "busybox", Cmd: cmd, Tty: true}
+	mounts := []mount.Mount{mount.Mount{
+		Type:     mount.TypeVolume,
+		Source:   cli.name,
+		Target:   mountpoint,
+		ReadOnly: true,
+	}}
+	hostcfg := docontainer.HostConfig{Mounts: mounts}
+	netcfg := network.NetworkingConfig{}
+	created, err := cli.ContainerCreate(ctx, &cfg, &hostcfg, &netcfg, "")
+	if err != nil {
+		return "", err
+	}
+	for _, warn := range created.Warnings {
+		log.Debugf("Warning creating %v: %v", created.ID, warn)
+	}
+	return created.ID, nil
 }
