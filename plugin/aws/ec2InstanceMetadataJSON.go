2215ec421cd263be25cd4f30c1f6b371f129f4a9
Enis Inan <enis.inan@puppet.com>
2019-03-13 12:19:09 -0700

    Add initial EC2 support to the AWS plugin
    This commit adds the initial EC2 support to the AWS plugin.
    It currently supports listing the available EC2 instances,
    retrieving their metadata, and checking their console output.
    The filesystem layout is resources/ec2/instances/<instance_id>/{console.out,console-latest.out,metadata.json}
    
    Exec support for EC2 instances will be added in a separate commit.

---
+++ plugin/aws/ec2InstanceMetadataJSON.go
@@ -0,0 +1,37 @@
+package aws
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+
+	"github.com/puppetlabs/wash/plugin"
+)
+
+// ec2InstanceMetadataJSON represents an EC2 instance's
+// metadata.json file
+type ec2InstanceMetadataJSON struct {
+	plugin.EntryBase
+	inst *ec2Instance
+}
+
+func newEC2InstanceMetadataJSON(inst *ec2Instance) *ec2InstanceMetadataJSON {
+	return &ec2InstanceMetadataJSON{
+		EntryBase: plugin.NewEntry("metadata.json"),
+		inst:      inst,
+	}
+}
+
+func (im *ec2InstanceMetadataJSON) Open(ctx context.Context) (plugin.SizedReader, error) {
+	metadata, err := im.inst.Metadata(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	content, err := json.MarshalIndent(metadata, "", "  ")
+	if err != nil {
+		return nil, err
+	}
+
+	return bytes.NewReader(content), nil
+}
