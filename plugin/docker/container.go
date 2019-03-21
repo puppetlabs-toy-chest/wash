puppetlabs-toy-chest/wash

commit e5f8c8025023d49281fb1feca7dee4794a854df2

Switch Docker plugin logging to use plugin journal

plugin/docker/container.go
@@ -68,6 +68,7 @@ func (c *container) Exec(ctx context.Context, cmd string, args []string, opts pl
 		defer resp.Close()
 
 		_, err := stdcopy.StdCopy(stdout, stderr, resp.Reader)
+		plugin.Log(ctx, "Exec on %v complete: %v", c.Name(), err)
 		stdout.CloseWithError(err)
 		stderr.CloseWithError(err)
 	}()
@@ -76,7 +77,7 @@ func (c *container) Exec(ctx context.Context, cmd string, args []string, opts pl
 	if opts.Stdin != nil {
 		go func() {
 			_, writeErr = io.Copy(resp.Conn, opts.Stdin)
-			plugin.LogErr(resp.CloseWrite())
+			plugin.Log(ctx, "Closed execution response stream for %v: %v", c.Name(), writeErr)
 		}()
 	}
 
@@ -95,6 +96,7 @@ func (c *container) Exec(ctx context.Context, cmd string, args []string, opts pl
 			return 0, fmt.Errorf("the command was marked as 'Running' even though the output streams reached EOF")
 		}
 
+		plugin.Log(ctx, "Exec on %v exited %v", c.Name(), resp.ExitCode)
 		return resp.ExitCode, nil
 	}
 

plugin/docker/container-log.go
@@ -9,8 +9,6 @@ import (
 	"github.com/docker/docker/client"
 	"github.com/docker/docker/pkg/stdcopy"
 	"github.com/puppetlabs/wash/plugin"
-
-	log "github.com/sirupsen/logrus"
 )
 
 type containerLogFile struct {
@@ -24,7 +22,7 @@ func (clf *containerLogFile) isTty(ctx context.Context) bool {
 	if err == nil {
 		return meta.Config.Tty
 	}
-	log.Infof("Error reading info for container %v: %v", clf.containerName, err)
+	plugin.Log(ctx, "Error reading info for container %v: %v", clf.containerName, err)
 	// Assume true so we don't try to process output if there was an error.
 	return true
 }
@@ -48,7 +46,7 @@ func (clf *containerLogFile) Open(ctx context.Context) (plugin.SizedReader, erro
 			return nil, err
 		}
 	}
-	log.Debugf("Read %v bytes of %v log", n, clf.containerName)
+	plugin.Log(ctx, "Read %v bytes of %v log", n, clf.containerName)
 
 	return bytes.NewReader(buf.Bytes()), nil
 }
@@ -67,9 +65,9 @@ func (clf *containerLogFile) Stream(ctx context.Context) (io.Reader, error) {
 	r, w := io.Pipe()
 	go func() {
 		if _, err = stdcopy.StdCopy(w, w, rdr); err != nil {
-			log.Debugf("Errored reading container %v: %v", clf.containerName, err)
+			plugin.Log(ctx, "Errored reading container %v: %v", clf.containerName, err)
 		}
-		plugin.LogErr(w.Close())
+		plugin.Log(ctx, "Closing write pipe: %v", w.Close())
 	}()
 	return r, nil
 }

plugin/docker/containers.go
@@ -7,7 +7,6 @@ import (
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 	"github.com/puppetlabs/wash/plugin"
-	log "github.com/sirupsen/logrus"
 )
 
 type containers struct {
@@ -22,7 +21,7 @@ func (cs *containers) List(ctx context.Context) ([]plugin.Entry, error) {
 		return nil, err
 	}
 
-	log.Debugf("Listing %v containers in %v", len(containers), cs)
+	plugin.Log(ctx, "Listing %v containers in %v", len(containers), cs)
 	keys := make([]plugin.Entry, len(containers))
 	for i, inst := range containers {
 		keys[i] = &container{

plugin/docker/volume.go
@@ -15,7 +15,6 @@ import (
 	"github.com/docker/docker/client"
 	"github.com/puppetlabs/wash/plugin"
 	vol "github.com/puppetlabs/wash/volume"
-	log "github.com/sirupsen/logrus"
 )
 
 type volume struct {
@@ -64,14 +63,17 @@ func (v *volume) List(ctx context.Context) ([]plugin.Entry, error) {
 	if err != nil {
 		return nil, err
 	}
-	defer func() { plugin.LogErr(v.client.ContainerRemove(ctx, cid, types.ContainerRemoveOptions{})) }()
+	defer func() {
+		err := v.client.ContainerRemove(ctx, cid, types.ContainerRemoveOptions{})
+		plugin.Log(ctx, "Deleted container %v: %v", cid, err)
+	}()
 
-	log.Debugf("Starting container %v", cid)
+	plugin.Log(ctx, "Starting container %v", cid)
 	if err := v.client.ContainerStart(ctx, cid, types.ContainerStartOptions{}); err != nil {
 		return nil, err
 	}
 
-	log.Debugf("Waiting for container %v", cid)
+	plugin.Log(ctx, "Waiting for container %v", cid)
 	waitC, errC := v.client.ContainerWait(ctx, cid, docontainer.WaitConditionNotRunning)
 	var statusCode int64
 	select {
@@ -79,7 +81,7 @@ func (v *volume) List(ctx context.Context) ([]plugin.Entry, error) {
 		return nil, err
 	case result := <-waitC:
 		statusCode = result.StatusCode
-		log.Debugf("Container %v finished[%v]: %v", cid, result.StatusCode, result.Error)
+		plugin.Log(ctx, "Container %v finished[%v]: %v", cid, result.StatusCode, result.Error)
 	}
 
 	opts := types.ContainerLogsOptions{ShowStdout: true}
@@ -87,12 +89,14 @@ func (v *volume) List(ctx context.Context) ([]plugin.Entry, error) {
 		opts.ShowStderr = true
 	}
 
-	log.Debugf("Gathering logs for %v", cid)
+	plugin.Log(ctx, "Gathering log for %v", cid)
 	output, err := v.client.ContainerLogs(ctx, cid, opts)
 	if err != nil {
 		return nil, err
 	}
-	defer func() { plugin.LogErr(output.Close()) }()
+	defer func() {
+		plugin.Log(ctx, "Closed log for %v: %v", cid, output.Close())
+	}()
 
 	if statusCode != 0 {
 		bytes, err := ioutil.ReadAll(output)
@@ -136,7 +140,7 @@ func (v *volume) createContainer(ctx context.Context, cmd []string) (string, err
 		return "", err
 	}
 	for _, warn := range created.Warnings {
-		log.Debugf("Warning creating %v: %v", created.ID, warn)
+		plugin.Log(ctx, "Warning creating %v: %v", created.ID, warn)
 	}
 	return created.ID, nil
 }
@@ -148,20 +152,28 @@ func (v *volume) getContentCB() vol.ContentCB {
 		if err != nil {
 			return nil, err
 		}
-		defer func() { plugin.LogErr(v.client.ContainerRemove(ctx, cid, types.ContainerRemoveOptions{})) }()
+		defer func() {
+			err := v.client.ContainerRemove(ctx, cid, types.ContainerRemoveOptions{})
+			plugin.Log(ctx, "Deleted temporary container %v: %v", cid, err)
+		}()
 
-		log.Debugf("Starting container %v", cid)
+		plugin.Log(ctx, "Starting container %v", cid)
 		if err := v.client.ContainerStart(ctx, cid, types.ContainerStartOptions{}); err != nil {
 			return nil, err
 		}
-		defer func() { plugin.LogErr(v.client.ContainerKill(ctx, cid, "SIGKILL")) }()
+		defer func() {
+			err := v.client.ContainerKill(ctx, cid, "SIGKILL")
+			plugin.Log(ctx, "Stopped temporary container %v: %v", cid, err)
+		}()
 
 		// Download file, then kill container.
 		rdr, _, err := v.client.CopyFromContainer(ctx, cid, mountpoint+path)
 		if err != nil {
 			return nil, err
 		}
-		defer func() { plugin.LogErr(rdr.Close()) }()
+		defer func() {
+			plugin.Log(ctx, "Closed file %v on %v: %v", mountpoint+path, cid, rdr.Close())
+		}()
 
 		tarReader := tar.NewReader(rdr)
 		// Only expect one file.

plugin/docker/volumes.go
@@ -6,7 +6,6 @@ import (
 	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/client"
 	"github.com/puppetlabs/wash/plugin"
-	log "github.com/sirupsen/logrus"
 )
 
 type volumes struct {
@@ -21,7 +20,7 @@ func (vs *volumes) List(ctx context.Context) ([]plugin.Entry, error) {
 		return nil, err
 	}
 
-	log.Debugf("Listing %v volumes in %v", len(volumes.Volumes), vs)
+	plugin.Log(ctx, "Listing %v volumes in %v", len(volumes.Volumes), vs)
 	keys := make([]plugin.Entry, len(volumes.Volumes))
 	for i, inst := range volumes.Volumes {
 		if keys[i], err = newVolume(vs.client, inst); err != nil {

commit 4bb26b74baf1488beda29fe39e85c378e6a5528f

Fix Docker exec with stdin
When exec included input, Docker exec would hang. This was a regression
introduced with commit e5f8c8025023d49281fb1feca7dee4794a854df2, where
it removed closing the write stream after copying input. Restore the
call to `CloseWrite`.

Signed-off-by: Michael Smith <michael.smith@puppet.com>

plugin/docker/container.go
@@ -78,7 +78,8 @@ func (c *container) Exec(ctx context.Context, cmd string, args []string, opts pl
 	if opts.Stdin != nil {
 		go func() {
 			_, writeErr = io.Copy(resp.Conn, opts.Stdin)
-			journal.Record(ctx, "Closed execution response stream for %v: %v", c.Name(), writeErr)
+			respErr := resp.CloseWrite()
+			journal.Record(ctx, "Closed execution input stream for %v: %v, %v", c.Name(), writeErr, respErr)
 		}()
 	}
 