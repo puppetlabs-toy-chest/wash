9a6af863e7310be8c6353608c0f0f4b8647ce817
Enis Inan <enis.inan@puppet.com>
2019-05-08 20:53:21 -0700

    Add expression.Parenthesize
    This commit adds the expression.Parenthesize combinator, which can be
    used to construct expression parsers that parse only parenthesized
    expressions.
    
    Signed-off-by: Enis Inan <enis.inan@puppet.com>

--- cmd/internal/find/parser/expression/atom.go
+++ cmd/internal/find/parser/expression/atom.go
@@ -4,7 +4,7 @@ import "github.com/puppetlabs/wash/cmd/internal/find/parser/predicate"
 import "github.com/puppetlabs/wash/cmd/internal/find/parser/errz"
 import "fmt"
 
-func notOpParser(parser *Parser) predicate.Parser {
+func notOpParser(parser Parser) predicate.Parser {
 	return predicate.ToParser(func(tokens []string) (predicate.Predicate, []string, error) {
 		notToken := tokens[0]
 		if notToken != "!" && notToken != "-not" {
@@ -15,12 +15,12 @@ func notOpParser(parser *Parser) predicate.Parser {
 			return nil, nil, fmt.Errorf("%v: no following expression", notToken)
 		}
 		if tokens[0] == ")" {
-			if parser.numOpenParens <= 0 {
+			if !parser.insideParens() {
 				return nil, nil, fmt.Errorf("): no beginning '('")
 			}
 			return nil, nil, fmt.Errorf("%v: no following expression", notToken)
 		}
-		p, tokens, err := parser.atom.Parse(tokens)
+		p, tokens, err := parser.atom().Parse(tokens)
 		if err != nil {
 			if errz.IsMatchError(err) {
 				err = fmt.Errorf("%v: no following expression", notToken)
@@ -31,29 +31,41 @@ func notOpParser(parser *Parser) predicate.Parser {
 	})
 }
 
-func parensOpParser(parser *Parser) predicate.Parser {
+// Parenthesize returns a predicate parser that only parses parenthesized expressions.
+// The expressions themselves are parsed by the given parser. Note that the parser
+// returned by Parenthesize mutates the passed-in parser's state.
+//
+// If Parser#Parse returns an EmptyExpressionError, then Parenthesize also returns an
+// EmptyExpressionError,
+func Parenthesize(parser Parser) predicate.Parser {
 	return predicate.ToParser(func(tokens []string) (predicate.Predicate, []string, error) {
+		if len(tokens) == 0 {
+			return nil, nil, errz.NewMatchError("expected an '('")
+		}
+		if tokens[0] == ")" {
+			return nil, nil, fmt.Errorf("): no beginning '('")
+		}
 		if tokens[0] != "(" {
 			return nil, nil, errz.NewMatchError("expected an '('")
 		}
 		tokens = tokens[1:]
-		parser.numOpenParens++
+		parser.openParens()
 		// Save the current evaluation stack. We will restore it after parsing
 		// the parenthesized expression
-		stack := parser.stack
+		stack := parser.stack()
 		defer func() {
-			parser.stack = stack
-			parser.numOpenParens--
+			parser.setStack(stack)
+			parser.closeParens()
 		}()
 		p, tokens, err := parser.Parse(tokens)
-		if err != nil && err != ErrEmptyExpression {
+		if err != nil && !IsEmptyExpressionError(err) {
 			return p, tokens, err
 		}
-		// err == nil || err == ErrEmptyExpression
+		// err == nil || IsEmptyExpressionError(err)
 		if len(tokens) == 0 || tokens[0] != ")" {
 			return nil, nil, fmt.Errorf("(: missing closing ')'")
 		}
-		if err == ErrEmptyExpression {
+		if IsEmptyExpressionError(err) {
 			return nil, nil, fmt.Errorf("(): empty inner expression")
 		}
 		tokens = tokens[1:]
--- cmd/internal/find/parser/expression/error.go
+++ cmd/internal/find/parser/expression/error.go
@@ -14,5 +14,23 @@ func (e UnknownTokenError) Error() string {
 	return fmt.Sprintf("unknown token %v", e.Token)
 }
 
-// ErrEmptyExpression represents an empty expression error
-var ErrEmptyExpression = fmt.Errorf("empty expression")
+// EmptyExpressionError represents an empty expression error.
+type EmptyExpressionError struct {
+	msg string
+}
+
+// NewEmptyExpressionError creates an EmptyExpressionError object
+func NewEmptyExpressionError(msg string) EmptyExpressionError {
+	return EmptyExpressionError{msg}
+}
+
+func (e EmptyExpressionError) Error() string {
+	return e.msg
+}
+
+// IsEmptyExpressionError returns true if err is an EmptyExpressionError
+// object, false otherwise.
+func IsEmptyExpressionError(err error) bool {
+	_, ok := err.(EmptyExpressionError)
+	return ok
+}
