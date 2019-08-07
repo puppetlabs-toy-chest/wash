08a7b67a267e5087ad0b8b1293c27ff2976933c5
Enis Inan <enis.inan@puppet.com>
2019-08-07 12:41:55 -0700

    Fix analytics config parsing
    This commit simplifies the logic a bit and avoids writing an analytics
    config file when analytics is disabled.
    
    Signed-off-by: Enis Inan <enis.inan@puppet.com>

--- analytics/config.go
+++ analytics/config.go
@@ -19,78 +19,72 @@ type Config struct {
 
 // GetConfig returns Wash's analytics config, which is located at
 // $HOME/.puppetlabs/wash/analytics.yaml.
-func GetConfig() (config Config, err error) {
-	// Check if analytics is disabled via the environment before
-	// returning
-	defer func() {
+func GetConfig() (Config, error) {
+	config := Config{}
+
+	// Check if analytics is disabled via the environment
+	if disabledStr, ok := os.LookupEnv("WASH_DISABLE_ANALYTICS"); ok {
+		disabledBool, err := strconv.ParseBool(disabledStr)
 		if err != nil {
-			return
-		}
-		disabledStr, ok := os.LookupEnv("WASH_DISABLE_ANALYTICS")
-		if !ok {
-			return
-		}
-		// Doing disabledBool, err := ... will not write to the err return
-		// value. It will instead create a new err variable within the defer
-		// func's scope. To avoid this, declare disabledBool so that we can
-		// use "=" instead of ":="
-		var disabledBool bool
-		disabledBool, err = strconv.ParseBool(disabledStr)
-		if err != nil {
-			err = fmt.Errorf("WASH_DISABLE_ANALYTICS is set to %v. Valid values are 'true' or 'false'", disabledStr)
-			return
+			return config, fmt.Errorf("WASH_DISABLE_ANALYTICS is set to %v. Valid values are 'true' or 'false'", disabledStr)
 		}
 		config.Disabled = disabledBool
-	}()
+		if config.Disabled {
+			// Analytics is disabled so we can return. There's no need to do anything
+			// else.
+			return config, nil
+		}
+	}
 
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
-		return
+		return config, err
 	}
 	analyticsConfigFile := filepath.Join(homeDir, ".puppetlabs", "wash", "analytics.yaml")
 	config, err = readAnalyticsConfigFile(analyticsConfigFile)
-	if err == nil || !os.IsNotExist(err) {
-		return
-	}
-	// The analyticsConfigFile does not exist, so create one. First, read
-	// Bolt's analytics config file (if it exists) so we can re-use its
-	// user ID
-	config, err = readAnalyticsConfigFile(
-		filepath.Join(homeDir, ".puppetlabs", "bolt", "analytics.yaml"),
-	)
 	if err != nil && !os.IsNotExist(err) {
-		return
+		return config, newConfigReadErr(analyticsConfigFile, err)
 	}
-	if config.UserID == uuid.Nil {
+	if config.Disabled || config.UserID != uuid.Nil {
+		return config, nil
+	}
+	// Analytics is not disabled, but no user ID is set. Thus, we'll need
+	// to generate a user ID and then write that user ID to the analytics
+	// config file. Note that this also handles the case of a nonexistant
+	// config file.
+	//
+	// First, try using Bolt's user ID if it's available.
+	boltAnalyticsConfigFile := filepath.Join(homeDir, ".puppetlabs", "bolt", "analytics.yaml")
+	boltAnalyticsConfig, err := readAnalyticsConfigFile(boltAnalyticsConfigFile)
+	if err != nil && !os.IsNotExist(err) {
+		return config, newConfigReadErr(boltAnalyticsConfigFile, err)
+	}
+	if boltAnalyticsConfig.UserID != uuid.Nil {
+		config.UserID = boltAnalyticsConfig.UserID
+	} else {
 		config.UserID = uuid.New()
 	}
-	config.Disabled = false
+	// Now write the config
 	bytes, err := yaml.Marshal(config)
 	if err != nil {
 		// This should never happen
-		err = fmt.Errorf("could not marshal the analytics config: %v", err)
-		return
+		return config, fmt.Errorf("could not marshal the analytics config: %v", err)
 	}
-	err = ioutil.WriteFile(analyticsConfigFile, bytes, 0644)
-	return
+	return config, ioutil.WriteFile(analyticsConfigFile, bytes, 0644)
 }
 
 func readAnalyticsConfigFile(path string) (Config, error) {
-	_, err := os.Stat(path)
+	rawConfig, err := ioutil.ReadFile(path)
 	if err != nil {
 		return Config{}, err
 	}
-	rawConfig, err := ioutil.ReadFile(path)
-	if err != nil {
-		return Config{}, fmt.Errorf("error reading %v: %v", path, err)
-	}
 	var config Config
 	if err := yaml.Unmarshal(rawConfig, &config); err != nil {
-		return config, fmt.Errorf(
-			"error reading %v: could not unmarshal the analytics config: %v",
-			path,
-			err,
-		)
+		return config, fmt.Errorf("could not unmarshal the analytics config: %v", err)
 	}
 	return config, nil
+}
+
+func newConfigReadErr(file string, reason error) error {
+	return fmt.Errorf("error reading %v: %v", file, reason)
 }
