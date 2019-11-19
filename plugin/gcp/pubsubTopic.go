0aa933bc6aad1cc7337b3325c240e82a32797847
Michael Smith <michael.smith@puppet.com>
2019-11-19 12:31:07 -0800

    Add ability to subscribe to topic
    Allow streaming from topics by creating a temporary subscription to that
    topic. Buffer messages to that subscription for reading, and delete it
    when the stream is closed.
    
    Signed-off-by: Michael Smith <michael.smith@puppet.com>

--- plugin/gcp/pubsubTopic.go
+++ plugin/gcp/pubsubTopic.go
@@ -1,9 +1,16 @@
 package gcp
 
 import (
+	"container/list"
 	"context"
+	"fmt"
+	"io"
+	"sync"
+	"time"
 
 	"cloud.google.com/go/pubsub"
+	"github.com/google/uuid"
+	"github.com/puppetlabs/wash/activity"
 	"github.com/puppetlabs/wash/plugin"
 	"google.golang.org/api/iterator"
 )
@@ -64,6 +71,83 @@ func (t *pubsubTopic) Metadata(ctx context.Context) (plugin.JSONObject, error) {
 
 func (t *pubsubTopic) Delete(ctx context.Context) (bool, error) {
 	return true, t.topic.Delete(ctx)
+}
+
+// A ReadCloser that subscribes to a topic and buffers all messages that appear there.
+// Uses a mutex to synchronize accessing the queue (List) and recording receive errors.
+// It shares this mutex because accessing the error is low cost and unlikely to happen
+// while we're still buffering messages.
+type pubsubTopicWatcher struct {
+	ctx   context.Context
+	sub   *pubsub.Subscription
+	mux   sync.Mutex
+	queue *list.List
+	err   error
+}
+
+func (t *pubsubTopic) newPubsubTopicWatcher(ctx context.Context) (*pubsubTopicWatcher, error) {
+	sub, err := t.client.CreateSubscription(ctx, "wash-"+uuid.New().String(), pubsub.SubscriptionConfig{
+		Topic:            t.topic,
+		AckDeadline:      10 * time.Second,
+		ExpirationPolicy: 24 * time.Hour,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	watcher := &pubsubTopicWatcher{ctx: ctx, sub: sub, queue: list.New()}
+
+	bufferMessages := func(_ context.Context, msg *pubsub.Message) {
+		msg.Ack()
+		watcher.mux.Lock()
+		watcher.queue.PushBack(msg)
+		watcher.mux.Unlock()
+	}
+	go func() {
+		err := sub.Receive(ctx, bufferMessages)
+		watcher.mux.Lock()
+		watcher.err = err
+		watcher.mux.Unlock()
+	}()
+	return watcher, nil
+}
+
+func (w *pubsubTopicWatcher) Read(p []byte) (int, error) {
+	// If there are outstanding messages, return one.
+	// If not, check if the context is done before returning.
+	var msg *pubsub.Message
+	w.mux.Lock()
+	if w.queue.Len() > 0 {
+		e := w.queue.Front()
+		w.queue.Remove(e)
+		msg = e.Value.(*pubsub.Message)
+	}
+	w.mux.Unlock()
+
+	activity.Record(w.ctx, "Reading next message: %v", msg)
+	if msg == nil {
+		select {
+		case <-w.ctx.Done():
+			return 0, io.EOF
+		default:
+			w.mux.Lock()
+			defer w.mux.Unlock()
+			return 0, w.err
+		}
+	}
+
+	// TODO: don't truncate messages longer than the read buffer.
+	s := fmt.Sprintf("%v | %v", msg.PublishTime.Format(time.StampMilli), string(msg.Data))
+	return copy(p, []byte(s)), nil
+}
+
+func (w *pubsubTopicWatcher) Close() error {
+	return w.sub.Delete(context.Background())
+}
+
+func (t *pubsubTopic) Stream(ctx context.Context) (io.ReadCloser, error) {
+	// Create subscription and wrap it in a ReadCloser to handle cleanup.
+	return t.newPubsubTopicWatcher(ctx)
 }
 
 func (t *pubsubTopic) Schema() *plugin.EntrySchema {
