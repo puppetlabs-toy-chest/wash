81fb5befcaa6a078a4aa3c327128081fb1609ec8
Enis Inan <enis.inan@puppet.com>
2019-12-12 14:50:34 -0800

    Add cmdutil.Prompt
    This is so we can use the stderrMux (and make it thread-safe).
    
    Signed-off-by: Enis Inan <enis.inan@puppet.com>

---
+++ cmd/util/prompt.go
@@ -0,0 +1,31 @@
+package cmdutil
+
+import (
+	"fmt"
+)
+
+// InputParser represents a parser that parses input
+// passed into Prompt.
+type InputParser = func(string) (interface{}, error)
+
+// YesOrNoP parses input representing confirmation. confirmed (bool) is
+// true if the input starts with "y" or "Y".
+var YesOrNoP InputParser = func(input string) (confirmed interface{}, err error) {
+	confirmed = len(input) > 0 && (input[0] == 'y' || input[0] == 'Y')
+	return
+}
+
+// Prompt prints the supplied message, waits for input on stdin,
+// then passes the input over to the supplied parser.
+func Prompt(msg string, parser InputParser) (interface{}, error) {
+	stderrMux.Lock()
+	defer stderrMux.Unlock()
+
+	var input string
+	fmt.Fprintf(Stderr, "%s: ", msg)
+	_, err := fmt.Scanln(&input)
+	if err != nil {
+		return nil, err
+	}
+	return parser(input)
+}
