c7a3bee5f3a123b79a824c164a2d4c20242df3e0
Enis Inan <enis.inan@puppet.com>
2020-01-13 19:17:16 -0800

    Update find with the new partial metadata changes
    Signed-off-by: Enis Inan <enis.inan@puppet.com>

--- cmd/internal/find/types/entry.go
+++ cmd/internal/find/types/entry.go
@@ -2,14 +2,12 @@ package types
 
 import (
 	apitypes "github.com/puppetlabs/wash/api/types"
-	"github.com/puppetlabs/wash/plugin"
 )
 
 // Entry represents an Entry as interpreted by `wash find`
 type Entry struct {
 	apitypes.Entry
 	NormalizedPath string
-	Metadata       plugin.JSONObject
 	SchemaKnown    bool
 	Schema         *EntrySchema
 }
@@ -19,7 +17,6 @@ func NewEntry(e apitypes.Entry, normalizedPath string) Entry {
 	return Entry{
 		Entry:          e,
 		NormalizedPath: normalizedPath,
-		Metadata:       e.Attributes.Meta(),
 	}
 }
 
