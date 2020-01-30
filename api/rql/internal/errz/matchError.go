71944e81b1742979a0110add8830e4d14b68a48e
Enis Inan <enis.inan@puppet.com>
2020-01-30 12:24:54 -0800

    Remove errz.IsSyntaxError, make MatchError pointer
    IsSyntaxError wasn't being used anywhere, and making MatchError a
    pointer is more consistent with other types that implement the error
    interface.
    
    Signed-off-by: Enis Inan <enis.inan@puppet.com>

--- api/rql/internal/errz/matchError.go
+++ api/rql/internal/errz/matchError.go
@@ -8,31 +8,18 @@ type MatchError struct {
 	reason string
 }
 
-func (m MatchError) Error() string {
+func (m *MatchError) Error() string {
 	return m.reason
 }
 
 // MatchErrorf creates a new MatchError object
-func MatchErrorf(format string, a ...interface{}) MatchError {
-	return MatchError{fmt.Sprintf(format, a...)}
+func MatchErrorf(format string, a ...interface{}) error {
+	return &MatchError{fmt.Sprintf(format, a...)}
 }
 
 // IsMatchError returns true if err is a MatchError,
 // false otherwise.
 func IsMatchError(err error) bool {
-	_, ok := err.(MatchError)
+	_, ok := err.(*MatchError)
 	return ok
-}
-
-// IsSyntaxError returns true if err is a syntax error, false otherwise.
-func IsSyntaxError(err error) bool {
-	if err == nil {
-		return false
-	}
-	switch err.(type) {
-	case MatchError:
-		return false
-	default:
-		return true
-	}
 }
