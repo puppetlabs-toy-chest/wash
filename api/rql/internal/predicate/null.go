f6c019590f134f265bace497ea9e5ec3d4716eb9
Enis Inan <enis.inan@puppet.com>
2020-01-30 02:23:43 -0800

    Implement the null predicate
    Signed-off-by: Enis Inan <enis.inan@puppet.com>

---
+++ api/rql/internal/predicate/null.go
@@ -0,0 +1,34 @@
+package predicate
+
+import (
+	"github.com/puppetlabs/wash/api/rql"
+	"github.com/puppetlabs/wash/api/rql/internal/errz"
+)
+
+func Null() rql.ValuePredicate {
+	return &null{}
+}
+
+type null struct{}
+
+func (p *null) Marshal() interface{} {
+	return nil
+}
+
+func (p *null) Unmarshal(input interface{}) error {
+	if input != nil {
+		return errz.MatchErrorf("must be null")
+	}
+	return nil
+}
+
+func (p *null) ValueInDomain(v interface{}) bool {
+	// Any value works with the null predicate
+	return true
+}
+
+func (p *null) EvalValue(v interface{}) bool {
+	return v == nil
+}
+
+var _ = rql.ValuePredicate(&null{})
