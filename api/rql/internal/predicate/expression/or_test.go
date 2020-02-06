8e32e9d1abf94867b796ba557909df3cc2b02ca0
Enis Inan <enis.inan@puppet.com>
2020-02-06 01:15:27 -0800

    Refactor expression tests to remove uses of predicate package
    This way, we can reuse the expression package in the predicate package.
    This will be necessary to properly implement the meta primary
    
    Signed-off-by: Enis Inan <enis.inan@puppet.com>

--- api/rql/internal/predicate/expression/or_test.go
+++ api/rql/internal/predicate/expression/or_test.go
@@ -3,114 +3,24 @@ package expression
 import (
 	"testing"
 
-	"github.com/puppetlabs/wash/api/rql"
-	"github.com/puppetlabs/wash/api/rql/ast/asttest"
-	"github.com/puppetlabs/wash/api/rql/internal/predicate"
-	"github.com/puppetlabs/wash/api/rql/internal/primary"
-	"github.com/puppetlabs/wash/plugin"
 	"github.com/stretchr/testify/suite"
 )
 
-type OrTestSuite struct {
-	asttest.Suite
-}
-
-func (s *OrTestSuite) TestMarshal() {
-	p := Or(predicate.Boolean(true), predicate.Boolean(false))
-	s.MTC(p, s.A("OR", predicate.Boolean(true).Marshal(), predicate.Boolean(false).Marshal()))
-}
-
-func (s *OrTestSuite) TestUnmarshal() {
-	p := Or(predicate.Boolean(false), predicate.Boolean(false))
-	s.UMETC(p, "foo", `formatted.*"OR".*<pe>.*<pe>`, true)
-	s.UMETC(p, s.A("OR", "foo", "bar", "baz"), `"OR".*<pe>.*<pe>`, false)
-	s.UMETC(p, s.A("OR"), "OR.*LHS.*RHS.*expression", false)
-	s.UMETC(p, s.A("OR", true), "OR.*LHS.*RHS.*expression", false)
-	s.UMETC(p, s.A("OR", "foo", true), "OR.*LHS.*Boolean", false)
-	s.UMETC(p, s.A("OR", true, "foo"), "OR.*RHS.*Boolean", false)
-	s.UMTC(p, s.A("OR", true, true), Or(predicate.Boolean(true), predicate.Boolean(true)))
-}
-
-func (s *OrTestSuite) TestEvalEntry() {
-	s.EEFTC(Or(primary.Boolean(false), primary.Boolean(false)), rql.Entry{})
-	s.EETTC(Or(primary.Boolean(false), primary.Boolean(true)), rql.Entry{})
-	s.EETTC(Or(primary.Boolean(true), primary.Boolean(false)), rql.Entry{})
-	s.EETTC(Or(primary.Boolean(true), primary.Boolean(true)), rql.Entry{})
-}
-
-func (s *OrTestSuite) TestEvalEntrySchema() {
-	s.EESFTC(Or(primary.Boolean(false), primary.Boolean(false)), &rql.EntrySchema{})
-	s.EESTTC(Or(primary.Boolean(false), primary.Boolean(true)), &rql.EntrySchema{})
-	s.EESTTC(Or(primary.Boolean(true), primary.Boolean(false)), &rql.EntrySchema{})
-	s.EESTTC(Or(primary.Boolean(true), primary.Boolean(true)), &rql.EntrySchema{})
-}
-
-func (s *OrTestSuite) TestEvalValue() {
-	// Note that we can't use predicate.Boolean(val) here because those return true if v == val
-	p := Or(predicate.NumericValue(predicate.LT, s.N("10")), predicate.NumericValue(predicate.GT, s.N("10")))
-	// p1 == false, p2 == false
-	s.EVFTC(p, float64(10))
-	// false, true
-	s.EVTTC(p, float64(11))
-	// true, false
-	s.EVTTC(p, float64(9))
-	// true, true
-	p.(*or).p2 = p.(*or).p1
-	s.EVTTC(p, float64(9))
-}
-
-func (s *OrTestSuite) TestEvalString() {
-	p := Or(predicate.StringValueEqual("one"), predicate.StringValueEqual("two"))
-	// p1 == false, p2 == false
-	s.ESFTC(p, "foo")
-	// false, true
-	s.ESTTC(p, "two")
-	// true, false
-	s.ESTTC(p, "one")
-	// true, true
-	p.(*or).p2 = p.(*or).p1
-	s.ESTTC(p, "one")
-}
-
-func (s *OrTestSuite) TestEvalNumeric() {
-	p := Or(predicate.NumericValue(predicate.LT, s.N("10")), predicate.NumericValue(predicate.GT, s.N("10")))
-	// p1 == false, p2 == false
-	s.ENFTC(p, s.N("10"))
-	// false, true
-	s.ENTTC(p, s.N("11"))
-	// true, false
-	s.ENTTC(p, s.N("9"))
-	// true, true
-	p.(*or).p2 = p.(*or).p1
-	s.ENTTC(p, s.N("9"))
-}
-
-func (s *OrTestSuite) TestEvalTime() {
-	p := Or(predicate.TimeValue(predicate.LT, s.TM(10)), predicate.TimeValue(predicate.GT, s.TM(10)))
-	// p1 == false, p2 == false
-	s.ETFTC(p, s.TM(10))
-	// false, true
-	s.ETTTC(p, s.TM(11))
-	// true, false
-	s.ETTTC(p, s.TM(9))
-	// true, true
-	p.(*or).p2 = p.(*or).p1
-	s.ETTTC(p, s.TM(9))
-}
-
-func (s *OrTestSuite) TestEvalAction() {
-	p := Or(predicate.Action(plugin.ExecAction()), predicate.Action(plugin.ListAction()))
-	// p1 == false, p2 == false
-	s.EAFTC(p, plugin.DeleteAction())
-	// false, true
-	s.EATTC(p, plugin.ListAction())
-	// true, false
-	s.EATTC(p, plugin.ExecAction())
-	// true, true
-	p.(*or).p2 = p.(*or).p1
-	s.EATTC(p, plugin.ExecAction())
-}
-
 func TestOr(t *testing.T) {
-	suite.Run(t, new(OrTestSuite))
+	s := new(BinOpTestSuite)
+	s.opName = "OR"
+	s.newOp = Or
+	s.testEvalMethod = func(s *BinOpTestSuite, RFTC TCRunFunc, RTTC TCRunFunc, constructV func(string) interface{}) {
+		p := Or(newMockP("1"), newMockP("2"))
+		// p1 == false, p2 == false
+		RFTC(s, p, constructV("3"))
+		// false, true
+		RTTC(s, p, constructV("2"))
+		// true, false
+		RTTC(s, p, constructV("1"))
+		// true, true
+		p.(*or).p2 = p.(*or).p1
+		RTTC(s, p, constructV("1"))
+	}
+	suite.Run(t, s)
 }
